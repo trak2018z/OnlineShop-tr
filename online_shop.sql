efaf901eaa92505aeb9419db7a66f4a153c736d2
Moszeusz6Pl <mateusz.jurasz@wp.pl>
2017-11-21 08:25:03 +0100

    Cleaned up dataBase. Changed all string compare methods to utf8_general_ci

--- online_shop.sql
+++ online_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 08 Lis 2017, 20:51
+-- Czas generowania: 21 Lis 2017, 08:14
 -- Wersja serwera: 10.1.28-MariaDB
 -- Wersja PHP: 7.1.10
 
@@ -31,12 +31,19 @@ SET time_zone = "+00:00";
 CREATE TABLE `orders` (
   `id` int(10) UNSIGNED NOT NULL,
   `userId` int(10) UNSIGNED NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
-  `items` text CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
-  `total` varchar(255) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `items` text CHARACTER SET utf8 NOT NULL,
+  `total` varchar(255) CHARACTER SET utf8 NOT NULL,
   `status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `orders`
+--
+
+INSERT INTO `orders` (`id`, `userId`, `name`, `email`, `items`, `total`, `status`) VALUES
+(1, 2, 'user1', 'user1@user.pl', '{\"0\":{\"id\":\"11\",\"name\":\"Banany\",\"weight\":\"0.2\",\"description\":\"S\\u0142odkie o pe\\u0142nym smaku.\",\"price\":\"0.75\",\"qty\":\"1\"}}', '0.75', 0);
 
 -- --------------------------------------------------------
 
@@ -46,27 +53,28 @@ CREATE TABLE `orders` (
 
 CREATE TABLE `products` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
-  `weight` varchar(255) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
-  `description` text CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
-  `price` varchar(255) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `weight` varchar(255) CHARACTER SET utf8  NOT NULL,
+  `description` text CHARACTER SET utf8 NOT NULL,
+  `price` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `thumb` varchar(255) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Zrzut danych tabeli `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `weight`, `description`, `price`) VALUES
-(11, 'Banany', '0.2', 'Słodkie o pełnym smaku.', '0.75'),
-(12, 'Cytryny', '1', 'Soczyste i orzeźwiające, świetne do napojów', '8.99'),
-(13, 'Mandarynki', '1', 'Soczyste i aromatyczne o wyrazistym smaku.', '3.99'),
-(14, 'Grejpfrut czerwony', '0.4', 'Słodko-kwaśny.', '4.49'),
-(15, 'Jabłka', '1', 'Jabłka polskie Ligol lekko kwaśne twarde.', '2.99'),
-(16, 'Gruszka zielona', '1', 'Słodka i soczysta.', '5.90'),
-(17, 'Winogrona białe', '1', 'Słodkie i chrupiące.', '9.99'),
-(18, 'Winogrona czerwone', '1', 'Winogrona czerwone', '10.99'),
-(19, 'Arbuz', '2', 'Soczysty i orzeźwiający.', '4,99'),
-(20, 'Melon', '2', 'Słodki orzeźwiający o kremowym miąższu.', '7.99');
+INSERT INTO `products` (`id`, `name`, `weight`, `description`, `price`, `thumb`) VALUES
+(1, 'Banany', '0.2', 'Słodkie o pełnym smaku.', '0.75', 'banan.jpg'),
+(2, 'Cytryny', '1', 'Soczyste i orzeźwiające, świetne do napojów', '8.99', 'cytryna.jpg'),
+(3, 'Mandarynki', '1', 'Soczyste i aromatyczne o wyrazistym smaku.', '3.99', 'mandarynki.jpg'),
+(4, 'Grejpfrut czerwony', '0.4', 'Słodko-kwaśny.', '4.49', 'grejpfrut_czerwony.jpg'),
+(5, 'Jabłka', '1', 'Jabłka polskie Ligol lekko kwaśne twarde.', '2.99', 'jablka ligol.jpg'),
+(6, 'Gruszka zielona', '1', 'Słodka i soczysta.', '5.90', 'gruszka zielona.jpg'),
+(7, 'Winogrona białe', '1', 'Słodkie i chrupiące.', '9.99', 'winogrona biale.jpg'),
+(8, 'Winogrona czerwone', '1', 'Winogrona czerwone', '10.99', 'winogrona_rozowe.jpg'),
+(9, 'Arbuz', '2', 'Soczysty i orzeźwiający.', '4,99', 'arbuz.jpg'),
+(10, 'Melon', '2', 'Słodki orzeźwiający o kremowym miąższu.', '7.99', 'melon.jpg');
 
 -- --------------------------------------------------------
 
@@ -76,10 +84,10 @@ INSERT INTO `products` (`id`, `name`, `weight`, `description`, `price`) VALUES
 
 CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
-  `role` varchar(255) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `role` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -87,10 +95,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `role`, `password`) VALUES
-(24, 'Jagoda', 'pjagoda94@gmail.com', 'user', 'Vs9o5Jg8CxApk'),
-(25, 'Mateusz', 'mateusz.jurasz@wp.pl', 'admin', 'Vs9o5Jg8CxApk'),
-(26, 'Barbara', 'b.pieta.1971@gmail.com', 'user', 'Vs9o5Jg8CxApk'),
-(27, 'Edyta', 'elinek0@o2.pl', 'user', 'Vs9o5Jg8CxApk');
+(1, 'admin', 'admin@admin.pl', 'admin', '$2y$10$fzR0YsPv20rZGaNkPqGdmOZduAY4pNqpaO1ZwatlguDyNpLoKEpKy'),
+(2, 'user1', 'user1@user.pl', 'user', '$2y$10$NvHfLWOvSfMuK2OWZJJ4ruphL8VhspfkCAjOoMe1j5cM7On4oXBD6');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -123,19 +129,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT dla tabeli `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT dla tabeli `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ograniczenia dla zrzutów tabel
